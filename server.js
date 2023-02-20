16a03e354c4128d5b49934cc77a4ddda9a744e5e
Humzas <humzas@umich.edu>
2023-02-20 14:44:31 -0500

    Fixed cli issues

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const mysql = require('mysql2');
-const inq = require('inquirer');
-const cTable = require('console.table');
-const {init} = require('./assets/helper/helper');
+const mysql = require("mysql2");
+const inq = require("inquirer");
+const cTable = require("console.table");
+const { init } = require("./assets/helper/helper");
 
 init();
